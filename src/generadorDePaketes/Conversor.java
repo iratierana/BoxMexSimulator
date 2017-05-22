de2ea4f157d33b1d06688a36b1820ef6eaad5bf0
mikelreto <mikel_reto@hotmail.com>
2017-05-22 08:33:58 +0200

    Conversor funciones terminadas

--- src/generadorDePaketes/Conversor.java
+++ src/generadorDePaketes/Conversor.java
@@ -1,5 +1,36 @@
 package generadorDePaketes;
 
+import DTO.Pakete;
+import DTO.Producto;
+import Demo.PaketeIce;
+import Demo.ProductoIce;
+
 public class Conversor {
+	
+	public Conversor(){
+		
+	}
+	
+	PaketeIce convertirPakete(Pakete pakete){
+		PaketeIce paketeIce = null;
+		paketeIce.estado = pakete.getEstado();
+		paketeIce.id = pakete.getId();
+		for(int i = 0; i < pakete.getListaProductos().size(); i++){
+			ProductoIce productoIce = convertirProducto(pakete.getListaProductos().get(i));
+			paketeIce.listaProductos.add(productoIce);
+		}
+		
+		return paketeIce;
+	}
+	
+	ProductoIce convertirProducto(Producto producto){
+		ProductoIce productoIce = null;
+		productoIce.categoriaId = producto.getCategoriaId();
+		productoIce.estanteriaId = producto.getEstanteriaId();
+		productoIce.fechaCaducidad = producto.getFechaCaducidad();
+		productoIce.nombre = producto.getNombre();
+		productoIce.productoId = producto.getProductoId();
+		return productoIce;
+	}
 
 }
